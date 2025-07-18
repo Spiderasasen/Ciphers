a9b4f92cbde36475517f66d115b19f81a88df178
Diego-Diaz797 <ddiaz11@elon.edu>
2025-07-18 10:56:01 -0400

    User Selection This is where the user can select if the user wants do something manuel or see the ai decypt some hard coded message. Finished working on the selection option of both manual or ai

--- Main.py
+++ Main.py
@@ -1,2 +1,29 @@
-#h
-print("Hello world!")
+# making boolean options
+option = False #if false its mainal
+language = ''
+
+#loop that will be used for selection
+while True:
+    #selecting the right option for the user
+    print('Which one will you like to do?')
+    print('1. User\n'
+          '2. Ai')
+
+    # var that will select the there option
+    choice = int(input())
+
+    #if the user slected 1, then the user can enerpt and decyept there own message
+    if choice == 1:
+        option = False
+        print('manial')
+        break
+
+    #if user selected 2, its ai
+    elif choice == 2:
+        option == True
+        print('ai')
+        break
+
+    #else, no option was selcted
+    else:
+        print('Please select a valid option')
