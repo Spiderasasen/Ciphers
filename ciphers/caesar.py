57bd40627119dfedca527a700651cb197b4d558c
Diego-Diaz797 <ddiaz11@elon.edu>
2025-07-18 13:07:08 -0400

    Caeser cipher Here it will be us working on the ceaser cipher section of the code. we are talking about the encryption, and decryption will be made first. after that we will work on the ai section of the project. might be using the encrption section for help Made the code to encrypt a message using the ceaser cipher.

--- ciphers/caesar.py
+++ ciphers/caesar.py
@@ -2,6 +2,9 @@
     if the option is false, its maunal
         if encrpytion is true its encption otherwise its decryption
 """
+import sys
+
+
 #checking if the option is manual or not
 def is_manual(option):
     #if option is true, then we enter manual
@@ -17,7 +20,34 @@ def manual(valid, encryption):
     if is_manual(valid):
         #checking if the code is encrpytion or not
         if encryption:
-            print(valid, "encryption")
+            #user puts there message
+            user_input = input('Please enter your message...\n')
+
+            #asking the number of shifts the user wants
+            while True:
+                #checking the user enter a valid number
+                try:
+                    shifts = int(input('\nHow many shifts would you want? \n1 - 25\n'))
+
+                    #if the number the user inputs is in between 1 - 25
+                    if 1 <= shifts <= 25:
+                        print(f'message: {user_input}, shifting: {shifts}')
+                        new_message = encryptions(user_input, shifts)
+                        print(new_message)
+                        break
+
+                    #if the user got a number outside of 1 - 25
+                    else:
+                        print('Please enter a valid number')
+
+                #if the error is just a normal fat finger
+                except ValueError:
+                    print('Please enter a valid number')
+
+                #anything else happens
+                except Exception as e:
+                    print(f'Error occurred: {e}')
+                    sys.exit(1)
 
         #we are decrpyting
         else:
@@ -25,5 +55,28 @@ def manual(valid, encryption):
 
     #if not manual then it return false
     return False
+
+#returns the message when encrpted depending on the number of shifts
+def encryptions(message: str, num_shifts: int):
+    encrypted_message = ''
+
+    #ensureing the shift is within a valid range (1 - 25)
+    actual_shift = num_shifts % 26
+
+    for char in message:
+        #calculating the new postion
+        if 'a' <= char <= 'z':
+            #lower case char
+            new_char_code = ((ord(char)) - ord('a') + actual_shift % 26) + ord('a')
+            encrypted_message += chr(new_char_code)
+        elif 'A' <= char <= 'z':
+            #upper case char
+            new_char_code = ((ord(char)) - ord('A') + actual_shift % 26) + ord('A')
+            encrypted_message += chr(new_char_code)
+        else:
+            #nothing to add, because its not an alphabet char
+            encrypted_message += char
+
+    return encrypted_message
 
 """AI Code"""
--- test.py
+++ test.py
@@ -5,9 +5,7 @@ from ciphers.caesar import *
 def main():
     #testing the mainual code
     manual('m', True)
-    manual('a', True)
     manual('m', False)
-    manual('a', False)
 
 if __name__ == "__main__":
     main()
