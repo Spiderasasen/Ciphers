3d7b19b544ae86119fca2bb7a39cc91d969240c6
Diego-Diaz797 <ddiaz11@elon.edu>
2025-08-01 11:20:24 -0400

    Making ROT13 Sections of the code since, i figured out that rot13 is just a ceaser cipher in a shift of 13, i will have to alter something. I need to find a way to just override some things, the encryption and the decrpytion. I fix the ROT13 secition of the code. its all done, but some it was a wired type of override. its just ceaser with a 13 shift

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 from ciphers.caesar import *
 from ciphers.morse_code import *
 from ciphers.atbash import *
-
+from ciphers.ROT13 import *
 
 def main():
     """Caeser code"""
@@ -21,8 +21,17 @@ def main():
     """Atbash code"""
     # manual_atbash('m', True)
     # manual_atbash('m', False)
-    ai_atbash('a')
+    # ai_atbash('a')
 
+    """ROT13"""
+    # word = rot13_encrypt('Hello World')
+    # print(word)
+    # rot13_manual('m', True)
+    # rot13_manual('a', True)
+    # rot13_manual('a', False)
+    # rot13_manual('m', False)
+    # ai_rot13('m')
+    # ai_rot13('a')
 
 if __name__ == "__main__":
     main()
